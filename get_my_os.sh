7c99b315b802679029f770e37bc0eb5141376154
hackmitel <none>
2019-03-23 21:21:40 -0700

    trying another thing

--- get_my_os.sh
+++ get_my_os.sh
@@ -5,4 +5,4 @@ with open("/etc/os-release", "r") as f:
     lines = [line for line in f.readlines() if line.startswith("NAME")]
     name = lines[0].split("=")[-1].strip("\n")
     name = re.sub(r'^"|"$', '', name)
-    print(name)
+    print(name,end='')
