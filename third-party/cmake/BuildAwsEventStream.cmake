d1a9271622f449ab8fd7fc5fe6a495423e10c9b6
hackmitel <none>
2019-03-23 20:01:20 -0700

    add OS check for AlpineLinux on each of the two files: BuildAwsCCommon.cmake and BuildAwsEventStream.cmake

--- third-party/cmake/BuildAwsEventStream.cmake
+++ third-party/cmake/BuildAwsEventStream.cmake
@@ -1,6 +1,10 @@
 # All the dependencies(aws-c-common and aws-checksums) should live in the same directory as aws-c-event-stream,
 # therefore we set rpath for aws-c-event-stream to "$ORIGIN".
 set(DEPS_RPATH "$ORIGIN")
+
+execute_process(COMMAND cat /etc/os-release | grep '^NAME' | cut -d= -f2 OUTPUT_VARIABLE MYOS)
+message(STATUS "MYOS: ${MYOS}")
+
 if(${TARGET_ARCH} STREQUAL ANDROID)
     ExternalProject_Add(AwsCEventStream
         PREFIX ${AWS_DEPS_BUILD_DIR}
@@ -41,7 +45,8 @@ elseif(TARGET_ARCH STREQUAL "APPLE" AND DEFINED CMAKE_OSX_ARCHITECTURES AND NOT 
         -DCMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}
         -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
         )
-else()
+
+elseif(${MYOS} STREQUAL "Alpine Linux")
     #tklebanoff added -DCMAKE_C_STANDARD_LIBRARIES="/usr/lib/libexecinfo.a" for Alpine
     ExternalProject_Add(AwsCEventStream
         PREFIX ${AWS_DEPS_BUILD_DIR}
@@ -54,6 +59,23 @@ else()
         -DCMAKE_PREFIX_PATH=${AWS_DEPS_INSTALL_DIR}
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         -DCMAKE_C_STANDARD_LIBRARIES="/usr/lib/libexecinfo.a"
+        -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+        -DCMAKE_INSTALL_RPATH=${DEPS_RPATH}
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+        -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        )
+
+else()
+    ExternalProject_Add(AwsCEventStream
+        PREFIX ${AWS_DEPS_BUILD_DIR}
+        GIT_REPOSITORY ${AWS_EVENT_STREAM_URL}
+        GIT_TAG ${AWS_EVENT_STREAM_SHA}
+        BUILD_IN_SOURCE 0
+        UPDATE_COMMAND ""
+        CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${AWS_DEPS_INSTALL_DIR}
+        -DCMAKE_PREFIX_PATH=${AWS_DEPS_INSTALL_DIR}
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
         -DCMAKE_INSTALL_RPATH=${DEPS_RPATH}
         -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
